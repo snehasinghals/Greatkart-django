1ea6d803b64aa44a949f304eff1b30db62e16be0
snehasinghal <1035singhal@gmail.com>
2026-03-19 10:31:48 +0530

    django authetication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -135,4 +135,16 @@ STATICFILES_DIRS=[
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/ 'media'
 
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS={
+    messages.ERROR:'danger',
+}
+
+# SMTP CONGIURATIION
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_PORT= 587
+EMAIL_HOST_USER= '12snehasinghal@gmail.com'
+EMAIL_HOST_PASSWORD= 'ptbeajrydsjsowqg'
+EMAIL_USE_TLS= True
+
 
